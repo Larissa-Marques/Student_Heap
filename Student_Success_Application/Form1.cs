74f4defe1b78fc2945701ce572f7cf44beafed13
Larissa-Marques <nautico_larissa@hotmail.com>
2022-10-26 19:14:48 -0500

    Testing the implemented max heap

--- Student_Success_Application/Form1.cs
+++ Student_Success_Application/Form1.cs
@@ -1,18 +1,36 @@
-using System.Windows.Forms.VisualStyles;
+using Student_Heap;
 
 namespace Student_Success_Application
 {
     public partial class Form1 : Form
     {
+        Max_heap heap = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void insert_student_to_heap(int sid) { 
+            if(heap == null)
+            {
+                Node root = new Node(sid);
+                heap = new Max_heap(root);
+            }
+            else
+            {
+                heap.insert(new Node(sid));
+                heap.bfs_traversal();
+            }
         }
 
         private void btn_add_student_Click(object sender, EventArgs e)
         {
             // When this button is cliked, the data will be displayed in the listview.
             int student_id = new Random().Next(10000000, 99999999);
+            int gpa = Convert.ToInt32(textBox_GPA.Text);
+
+            // Adding to the heap
+            insert_student_to_heap(gpa);
 
             string[] row = { student_id.ToString(), textBox_student_name.Text, comboBox_major.Text,
                     comboBox_year.Text, textBox_GPA.Text};
@@ -27,7 +45,7 @@ namespace Student_Success_Application
             comboBox_major.Text = "";
             comboBox_year.Text = "";
             textBox_GPA.Text = "";
-            }
+        }
 
         private void btn_remove_record_Click(object sender, EventArgs e)
         {
